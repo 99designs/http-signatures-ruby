f332f92c5f7e22a5d627caf13e1f20abcf9de662
Paul Annesley <paul@annesley.cc>
2014-07-28 13:30:22 -0700

    RSpec configured.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |c|
+
+  c.color = true
+
+  c.default_formatter = "documentation"
+
+  c.disable_monkey_patching!
+
+end
