780ba271729e8a9e607a589266e414b8aa6ee95a
Paul Annesley <paul@annesley.cc>
2014-07-28 18:58:04 -0700

    KeyStore#[]=(id, secret) is private for now.
    Not yet used externally. May be public in future.

--- lib/http_signatures/context.rb
+++ lib/http_signatures/context.rb
@@ -25,12 +25,14 @@ module HttpSignatures
         key_hash.each { |id, secret| self[id] = secret }
       end
 
-      def []=(id, secret)
-        @keys[id] = HttpSignatures::Key.new(id: id, secret: secret)
-      end
-
       def fetch(id)
         @keys.fetch(id)
+      end
+
+      private
+
+      def []=(id, secret)
+        @keys[id] = HttpSignatures::Key.new(id: id, secret: secret)
       end
 
     end
