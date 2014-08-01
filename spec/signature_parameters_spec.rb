cb6213fce4f05cf7980b1c2afa35a205a0c62fff
Paul Annesley <paul@annesley.cc>
2014-08-01 12:04:26 -0700

    signature_parameters_spec.

---
+++ spec/signature_parameters_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe HttpSignatures::SignatureParameters do
+
+  subject(:signature_parameters) do
+    HttpSignatures::SignatureParameters.new(
+      key: key,
+      algorithm: algorithm,
+      header_list: header_list,
+      signature: signature,
+    )
+  end
+
+  let(:key) { double("Key", id: "pda") }
+  let(:algorithm) { double("Algorithm", name: "hmac-test") }
+  let(:header_list) { double("HeaderList", to_s: "a b c") }
+  let(:signature) { "sigstring" }
+
+  it "builds parameters into string" do
+    expect(signature_parameters.to_str).to eq(
+      'keyId="pda",algorithm="hmac-test",headers="a b c",signature="c2lnc3RyaW5n"'
+    )
+  end
+
+end
