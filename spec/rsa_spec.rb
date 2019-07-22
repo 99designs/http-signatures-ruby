7803afef0764e858b18758dc18cef0d393cfbab2
Paul Annesley <paul@annesley.cc>
2019-07-22 10:40:32 +1000

    RSA spec from @robjacoby showing verification failure

--- spec/rsa_spec.rb
+++ spec/rsa_spec.rb
@@ -14,6 +14,12 @@ RSpec.shared_examples_for "verifier" do
     context.signer.sign message
     expect(context.verifier.valid? message).to eq(true)
   end
+
+  it "rejects if a signed header has changed" do
+    context.signer.sign message
+    message["Date"] = "Thu, 12 Jan 2012 21:31:40 GMT"
+    expect(context.verifier.valid? message).to eq(false)
+  end
 end
 
 RSpec.describe "Using RSA" do
